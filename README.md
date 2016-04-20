89f26b145b69e6bd96197c217ffd7fc44f6dcc71
nitriques <nico@nitriques.com>
2016-04-19 21:51:44 -0400

    Initial commit. Skeleton extension

---
+++ README.md
@@ -0,0 +1,39 @@
+# Subresource Integrity
+
+Version: 1.0.x
+
+> A simple way to compute base64 encoded sha (256, 384, 512) of assets files used in `link` and `script` tags.
+
+### SPECS
+
+- You specify the files in a xml file (`manifest/sri.xml`)
+- The results are in the provided data source
+
+See <https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity> for more details
+
+### REQUIREMENTS
+
+- Symphony CMS version 2.6.0 and up (as of the day of the last release of this extension)
+
+### INSTALLATION
+
+- `git clone` / download and unpack the tarball file
+- Put into the extension directory
+- Enable/install just like any other extension
+
+You can also install it using the [extension downloader](http://symphonyextensions.com/extensions/extension_downloader/).
+Just search for `sri`.
+
+For more information, see <http://getsymphony.com/learn/tasks/view/install-an-extension/>
+
+### HOW TO USE
+
+TODO
+
+### LICENSE
+
+[MIT](http://deuxhuithuit.mit-license.org)
+
+Made with love in Montréal by [Deux Huit Huit](https://deuxhuithuit.com)
+
+Copyright (c) 2016
---
+++ extension.driver.php
@@ -0,0 +1,59 @@
+<?php
+	/*
+	Copyight: Deux Huit Huit 2016
+	LICENCE: MIT http://deuxhuithuit.mit-license.org;
+	*/
+	
+	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+	
+	/**
+	 *
+	 * @author Deux Huit Huit
+	 * https://deuxhuithuit.com/
+	 *
+	 */
+	class extension_sri extends Extension {
+
+		/**
+		 * Name of the extension
+		 * @var string
+		 */
+		const EXT_NAME = 'Subresource Integrity';
+
+		/* ********* INSTALL/UPDATE/UNINSTALL ******* */
+
+		/**
+		 * Creates the table needed for the settings of the field
+		 */
+		public function install() {
+			return true;
+		}
+
+		/**
+		 * This method will update the extension according to the
+		 * previous and current version parameters.
+		 * @param string $previousVersion
+		 */
+		public function update($previousVersion = false) {
+			$ret = true;
+			
+			if (!$previousVersion) {
+				$previousVersion = '0.0.1';
+			}
+			
+			// less than 0.0.1
+			if ($ret && version_compare($previousVersion, '0.0.1', '<')) {
+				
+			}
+			
+			return $ret;
+		}
+
+		/**
+		 * Drops the table needed for the settings of the field
+		 */
+		public function uninstall() {
+			return true;
+		}
+
+	}
